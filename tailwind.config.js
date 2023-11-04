a5af91a9899a5943d808b08cc2b1719bcb371a08
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-04 15:18:01 -0300

    feat: Update Tailwind config with  quotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.tsx'],
+  content: ["./index.html", "./src/**/*.tsx"],
   theme: {
     fontFamily: {
-      sans: ['Nunito', 'sans-serif'],
+      sans: ["Nunito", "sans-serif"],
     },
     extend: {
       colors: {
-        blue: '#3294F8',
+        blue: "#3294F8",
         base: {
-          title: '#E7EDF4',
-          subtitle: '#C4D4E3',
-          text: '#AFC2D4',
-          span: '#7B96B2',
-          label: '#3A536B',
-          border: '#1C2F41',
-          post: '#112131',
-          profile: '#0B1B2B',
-          background: '#071422',
-          input: '#040F1A',
+          title: "#E7EDF4",
+          subtitle: "#C4D4E3",
+          text: "#AFC2D4",
+          span: "#7B96B2",
+          label: "#3A536B",
+          border: "#1C2F41",
+          post: "#112131",
+          profile: "#0B1B2B",
+          background: "#071422",
+          input: "#040F1A",
         },
       },
     },
