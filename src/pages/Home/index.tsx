6225c7e20c7c8cbf2c23865569781ba656d281f7
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-04 16:11:13 -0300

    refactor: API usage and update Post interface.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { PostCard } from "@/components/PostCard";
 import { ProfileCard } from "@/components/ProfileCard";
-import { api } from "@/lib/api";
-import { useEffect, useState } from "react";
+import { useAPI } from "@/hooks/useAPI";
 
 interface User {
   login: string;
@@ -9,7 +8,7 @@ interface User {
   html_url: string;
 }
 
-export interface Post {
+export interface Posts {
   title: string;
   html_url: string;
   number: number;
@@ -19,47 +18,38 @@ export interface Post {
 }
 
 export function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    async function loadPosts() {
-      try {
-        const response = await api.get<Post[]>(
-          "/repos/pdro-lucas/github-blog/issues"
-        );
-
-        setPosts(response.data);
-      } catch (error) {
-        console.error("Failed to load posts", error);
-      }
-    }
-
-    loadPosts();
-  }, []);
+  const { data: posts, isLoading } = useAPI<Posts[]>(
+    "/repos/pdro-lucas/github-blog/issues"
+  );
+  console.log(posts);
 
   return (
     <main className="w-full max-w-4xl px-4 mx-auto">
       <ProfileCard />
 
-      <div className="mt-16 space-y-12">
-        <div className="flex flex-wrap items-center justify-between gap-2">
-          <span className="text-lg font-bold text-base-subtitle">
-            Publicações
-          </span>
-          <span className="text-sm text-base-span">
-            {posts.length} Publicaç{posts.length > 1 ? "ões" : "ão"}
-          </span>
-          <input
-            type="text"
-            className="w-full px-4 py-3 border rounded-md outline-none bg-base-input border-base-border placeholder:text-base-border"
-            placeholder="Buscar conteúdo"
-          />
-        </div>
+      {isLoading && <p>Carregando...</p>}
 
-        {posts.map((post) => (
-          <PostCard key={post.number} post={post} />
-        ))}
-      </div>
+      {posts && (
+        <div className="mt-16 space-y-12">
+          <div className="flex flex-wrap items-center justify-between gap-2">
+            <span className="text-lg font-bold text-base-subtitle">
+              Publicações
+            </span>
+            <span className="text-sm text-base-span">
+              {posts.length} Publicaç{posts.length > 1 ? "ões" : "ão"}
+            </span>
+            <input
+              type="text"
+              className="w-full px-4 py-3 border rounded-md outline-none bg-base-input border-base-border placeholder:text-base-border"
+              placeholder="Buscar conteúdo"
+            />
+          </div>
+
+          {posts.map((post) => (
+            <PostCard key={post.number} post={post} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
