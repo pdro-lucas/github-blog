66991b6cc4fa8402aafc3848a4fb604632cb3e04
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-04 01:07:20 -0300

    feat: Add ProfileCard component to Home page.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { ProfileCard } from "@/components/ProfileCard";
+
+export function Home() {
+  return (
+    <main className="w-full max-w-4xl mx-auto">
+      <ProfileCard />
+    </main>
+  );
+}
