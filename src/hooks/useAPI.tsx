f6ca8cb6cbf723daa7fd1e92e2bd704b15e8ea76
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-05 15:41:50 -0300

    feat: Refactor useAPI hook to handle type issue and improve code readability.

--- src/hooks/useAPI.tsx
+++ src/hooks/useAPI.tsx
@@ -2,31 +2,39 @@ import { api } from "@/lib/api";
 import { AxiosRequestConfig } from "axios";
 import { useCallback, useEffect, useState } from "react";
 
-export function useAPI<T>(url: string, config?: AxiosRequestConfig) {
+export function useAPI<T>(fetchUrl: string, config?: AxiosRequestConfig) {
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
+  const fetchData = useCallback(
+    async (url = fetchUrl) => {
+      setIsLoading(true);
 
-    try {
-      const response = await api.get<T>(url, config);
-      setData(response.data);
-    } catch (e) {
-      if (e instanceof Error) {
-        setError(e);
-      } else {
-        setError(new Error("An unknown error occurred"));
+      try {
+        // TODO - resolve type issue
+        const response = await api.get<T>(url, config);
+        if (response.data.items) {
+          setData(response.data.items);
+          return;
+        }
+        setData(response.data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e);
+        } else {
+          setError(new Error("An unknown error occurred"));
+        }
+      } finally {
+        setIsLoading(false);
       }
-    } finally {
-      setIsLoading(false);
-    }
-  }, [url, config]);
+    },
+    [config, fetchUrl]
+  );
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  return { data, isLoading, error };
+  return { data, isLoading, error, fetchData };
 }
