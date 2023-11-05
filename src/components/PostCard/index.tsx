1a6173e4e00e8dada38ac05252f10e98e603fb5d
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-05 15:42:01 -0300

    feat: Refactor PostCard component to improve readability.

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -9,21 +9,17 @@ interface PostCardProps {
 
 export function PostCard({ post }: PostCardProps) {
   return (
-    <div className="flex flex-wrap gap-8">
-      <div className="p-8 bg-base-post rounded-xl w-[416px] space-y-5">
-        <div className="flex items-baseline justify-between gap-6">
-          <span className="text-xl font-bold text-base-title">
-            {post.title}
-          </span>
-          <span className="text-xs capitalize whitespace-nowrap text-base-span">
-            {formatDistanceToNowStrict(new Date(post.created_at), {
-              locale: ptBR,
-              addSuffix: true,
-            })}
-          </span>
-        </div>
-        <Markdown className="line-clamp-4">{post.body}</Markdown>
+    <div className="p-8 bg-base-post rounded-xl w-[416px] space-y-5">
+      <div className="flex items-baseline justify-between gap-6">
+        <span className="text-xl font-bold text-base-title">{post.title}</span>
+        <span className="text-xs capitalize whitespace-nowrap text-base-span">
+          {formatDistanceToNowStrict(new Date(post.created_at), {
+            locale: ptBR,
+            addSuffix: true,
+          })}
+        </span>
       </div>
+      <Markdown className="line-clamp-4">{post.body}</Markdown>
     </div>
   );
 }
