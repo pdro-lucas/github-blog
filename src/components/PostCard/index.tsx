9fd03cc8aebe9cc5fcc17c508cdb8f81c051d121
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-06 01:18:43 -0300

    feat: Add NavLink to PostCard component

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -2,6 +2,7 @@ import { Post } from "@/pages/Home";
 import { formatDistanceToNowStrict } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Markdown from "react-markdown";
+import { NavLink } from "react-router-dom";
 
 interface PostCardProps {
   post: Post;
@@ -9,17 +10,26 @@ interface PostCardProps {
 
 export function PostCard({ post }: PostCardProps) {
   return (
-    <div className="p-8 bg-base-post rounded-xl w-[416px] space-y-5">
-      <div className="flex items-baseline justify-between gap-6">
-        <span className="text-xl font-bold text-base-title">{post.title}</span>
-        <span className="text-xs capitalize whitespace-nowrap text-base-span">
-          {formatDistanceToNowStrict(new Date(post.created_at), {
-            locale: ptBR,
-            addSuffix: true,
-          })}
-        </span>
+    <NavLink to={`/post/${post.number}`} className="flex-1">
+      <div className="p-8 bg-base-post rounded-xl w-[416px] space-y-5 h-64 hover:brightness-125 transition-all">
+        <div className="flex items-baseline justify-between gap-6">
+          <span className="text-xl font-bold text-base-title line-clamp-2">
+            {post.title}
+          </span>
+          <span className="text-xs capitalize whitespace-nowrap text-base-span">
+            {formatDistanceToNowStrict(new Date(post.created_at), {
+              locale: ptBR,
+              addSuffix: true,
+            })}
+          </span>
+        </div>
+        <Markdown
+          disallowedElements={["a"]}
+          className="prose line-clamp-4 prose-md text-base-text prose-headings:text-base-title"
+        >
+          {post.body || "# Sem conteúdo"}
+        </Markdown>
       </div>
-      <Markdown className="line-clamp-4">{post.body}</Markdown>
-    </div>
+    </NavLink>
   );
 }
